ef75e739095207810148078052648fb4cfb5ec60
bill donner <billdonner@gmail.com>
2019-11-26 21:03:45 -0500

    Initial Commit

---
+++ gsvconverter/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  gsvconverter
+//
+//  Created by bill donner on 11/26/19.
+//  Copyright © 2019 bill donner. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
